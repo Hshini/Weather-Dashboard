00171030b02369ce75d8189f45b8ead4552cce9c
Shini <halimshini@hotmail.com>
2023-02-22 00:24:11 -0500

    update of recent search

--- Assets/JavaScript/script.js
+++ Assets/JavaScript/script.js
@@ -1,2 +1,16 @@
 //Dependencies
-var searchBtn=document.querySelector('#search-btn')
+searchArea=document.querySelector("search-area")
+var searchBtn=document.querySelector('#search-btn');
+
+var city="london";
+var APIKey = "3ceee4a866c36aa0df2373e3c69834c3";
+
+var requestUrl="api.openweathermap.org/data/2.5/forecast?q=" + city + "&appid=" + APIKey;
+fetch(requestUrl)
+    .then(function (response) {
+      return response.json();
+    })
+    .then(function (data) {
+      console.log(data)
+  
+    })
