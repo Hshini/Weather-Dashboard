8c8111a5a799aa19c30d7db372f0d0db952239b3
Shini <halimshini@hotmail.com>
2023-02-22 23:58:45 -0500

    fix on weather api

--- Assets/JavaScript/script.js
+++ Assets/JavaScript/script.js
@@ -1,16 +1,34 @@
 //Dependencies
-searchArea=document.querySelector("search-area")
+var searchArea=document.querySelector("#search-area")
 var searchBtn=document.querySelector('#search-btn');
 
-var city="london";
+var city;
 var APIKey = "3ceee4a866c36aa0df2373e3c69834c3";
+var requestUrl;
 
-var requestUrl="api.openweathermap.org/data/2.5/forecast?q=" + city + "&appid=" + APIKey;
-fetch(requestUrl)
+function getWeather(cityname) {
+
+  requestUrl = "https://api.openweathermap.org/data/2.5/forecast?q=" + cityname + "&appid=" + APIKey+"&units=imperial";
+  fetch(requestUrl)
     .then(function (response) {
       return response.json();
     })
     .then(function (data) {
       console.log(data)
-  
+      console.log(data.list[0].wind.speed)
+      console.log(data.list[0].main.temp)
     })
+
+
+}
+
+
+    function search(event){
+      event.preventDefault()
+      console.log("hello")
+      console.log(searchArea.value);
+      city=searchArea.value;
+      getWeather(searchArea.value)
+
+      }
+    searchBtn.addEventListener("click",search);
