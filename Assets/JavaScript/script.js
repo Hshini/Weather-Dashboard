0d5a6d7dfbc1edd1cb99df054dc92e4b09c3c888
Shini <halimshini@hotmail.com>
2023-02-23 14:39:11 -0500

    fix on display City Name

--- Assets/JavaScript/script.js
+++ Assets/JavaScript/script.js
@@ -1,34 +1,50 @@
 //Dependencies
-var searchArea=document.querySelector("#search-area")
+var searchArea=document.querySelector("#search-area");
 var searchBtn=document.querySelector('#search-btn');
-
-var city;
+var temp=document.querySelector('#temp');
+var wind=document.querySelector('#wind');
+var humidity=document.querySelector('#humidity')
+var displayCity=document.querySelector("#display-city")
 var APIKey = "3ceee4a866c36aa0df2373e3c69834c3";
-var requestUrl;
+var data;
+var city;
 
+ 
+// Get info from openweather Api
 function getWeather(cityname) {
 
-  requestUrl = "https://api.openweathermap.org/data/2.5/forecast?q=" + cityname + "&appid=" + APIKey+"&units=imperial";
+  var requestUrl = "https://api.openweathermap.org/data/2.5/forecast?q=" + cityname + "&appid=" + APIKey+"&units=imperial";
   fetch(requestUrl)
     .then(function (response) {
       return response.json();
     })
     .then(function (data) {
-      console.log(data)
+      displayCity.textContent=city;
+      temp.textContent="Temp:" + data.list[0].main.temp;
+      wind.textContent="wind:" + data.list[0].wind.speed;
+      humidity.textContent="Humdity:" + data.list[0].main.humidity;
+      
+      console.log(data.list[0].main.humidity)
       console.log(data.list[0].wind.speed)
       console.log(data.list[0].main.temp)
+      
+      
+      
     })
+    
+   
 
 
 }
 
 
-    function search(event){
-      event.preventDefault()
-      console.log("hello")
-      console.log(searchArea.value);
-      city=searchArea.value;
-      getWeather(searchArea.value)
-
-      }
-    searchBtn.addEventListener("click",search);
+// User input
+function search(event){
+  event.preventDefault();
+  console.log(searchArea.value);
+  city=searchArea.value;
+  
+  getWeather(city);
+  
+}
+ searchBtn.addEventListener("click",search);
